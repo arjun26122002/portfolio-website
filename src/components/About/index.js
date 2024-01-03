edf61b6348126a67412e3f534b0fbbc3c4056445
Arjun Dey <arjun.dey.2612@gmail.com>
2024-01-03 21:35:02 +0530

    new

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import AnimatedLetters from "../AnimatedLetters"
 // import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
@@ -5,8 +6,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCss3, faGitAlt, faHtml5, faJava, faJsSquare, faReact } from "@fortawesome/free-brands-svg-icons"
 import Loader from 'react-loaders'
 import "./index.scss"
+
 // import LogoS from "../../assets/images/Resume.pdf"
-import LogoS from "../../assets/images/Resume.pdf"
+import Resums from "../../assets/images/Resume.pdf"
 const About = () => {
 
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -28,21 +30,24 @@ const About = () => {
             />
           </h1>
           <div className="updateP">
-          <p >
-          I am from Howrah,West Bengal. currently pursuing Bachelor of Technology major in computer science and Engineering. I am a student of 3rd year and passionate Front-End devaloper.
-          </p>
-          <p align="LEFT">I'm a very ambitious front-end developer looking for a role in an
-            established IT company with the opportunity to work with the latest
-            technologies on challenging and diverse projects.</p>
-          <p>I'm quiet confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.</p>
-            </div>
-            {/* <Link to="/download" className="flat-button">
+            <p >
+              I am from Howrah,West Bengal. currently pursuing Bachelor of Technology major in computer science and Engineering. I am a student of 4th year and passionate MERN devaloper.
+            </p>
+            <p align="LEFT">I'm a very ambitious MERN developer looking for a role in an
+              established IT company with the opportunity to work with the latest
+              technologies on challenging and diverse projects.</p>
+            <p>I'm quiet confident, naturally curious, and perpetually working on
+              improving my chops one design problem at a time.</p>
+          </div>
+          {/* <Link to="/download" className="flat-button">
             watch cv
           </Link> */}
-          <a download ="Resume.pdf" href={LogoS} class="flat-button">Download Resume</a>
+          {/* <a download="Resume.pdf" href={Resums} className="flat-button">Download Resume</a> */}
+          <a href="https://drive.google.com/drive/u/1/folders/182uFLsdPmlFdGoHBcLF_T4x-BTOI-vdY"className="flat-button" target='_blank' rel="noreferrer" style={{ textDecoration: "none" }}>
+           Click to see the Resume
+                    </a>
         </div>
-        
+
 
         <div className="stage-cube-cont">
           <div className="cubespinner">
@@ -66,19 +71,19 @@ const About = () => {
               <FontAwesomeIcon icon={faJava} color="#8A2BE2" />
             </div>
 
-          
+
 
 
           </div>
 
         </div>
-           
-        
-
-      </div>
 
 
-      
+
+      </div >
+
+
+
       <Loader type="pacman" />
     </>
 
